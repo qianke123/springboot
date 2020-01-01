ce0f3721ae531e6eef7f6e85f098ce67b4dd17c2
qianke123 <1150836154@qq.com>
2020-01-01 12:14:24 +0800

    springboot整合activeMq

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -3,9 +3,11 @@ package com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-// @EnableEurekaClient
+//@EnableEurekaClient
+//@EnableScheduling
 public class Application {
 
     public static void main(String[] args) {
--- src/main/java/com/how2java/activeMq/producer/QueueProducer.java
+++ src/main/java/com/how2java/activeMq/producer/QueueProducer.java
@@ -1,13 +1,19 @@
 package com.how2java.activeMq.producer;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsMessagingTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import javax.jms.Queue;
 
 @Component
 public class QueueProducer {
+    private static int id;
+
+    private Logger logger = Logger.getLogger(QueueProducer.class);
+
     @Autowired
     private JmsMessagingTemplate jmsMessagingTemplate;
 
@@ -16,5 +22,14 @@ public class QueueProducer {
 
     public void produceMsg() {
         jmsMessagingTemplate.convertAndSend(queue, "hello springBoot-activeMq");
+    }
+
+    /**
+     * 定时投递消息
+     */
+    // @Scheduled(fixedDelay = 10000)
+    public void produceMsgScheduled() {
+        jmsMessagingTemplate.convertAndSend(queue, "Scheduled " + (id++) + " hello springBoot-activeMq");
+        logger.info("定时投递第" + id + "个消息成功");
     }
 }
