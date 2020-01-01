203016bc5e5ba5898807e7fdf3812d30b0405fcd
qianke123 <1150836154@qq.com>
2020-01-01 10:10:29 +0800

    init

---
+++ src/main/java/com/how2java/activeMq/producer/QueueProducer.java
@@ -0,0 +1,20 @@
+package com.how2java.activeMq.producer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsMessagingTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.jms.Queue;
+
+@Component
+public class QueueProducer {
+    @Autowired
+    private JmsMessagingTemplate jmsMessagingTemplate;
+
+    @Autowired
+    private Queue queue;
+
+    public void produceMsg() {
+        jmsMessagingTemplate.convertAndSend(queue, "hello springBoot-activeMq");
+    }
+}
